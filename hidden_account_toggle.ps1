67d6ae9edaef1966a046de2747db5684674b184b
RecyclableBag <88467147+RecyclableBag@users.noreply.github.com>
2022-05-24 09:12:06 -0500

    Add files via upload

---
+++ hidden_account_toggle.ps1
@@ -0,0 +1,14 @@
+﻿$HideValue = get-itemproperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList'| Select-Object -ExpandProperty <$AccountName>  
+
+   if ($HideValue)
+    {
+    echo "<$AccountName> account is unhidden. Hiding it now."
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name <$AccountName> -Value 0 -Force
+    }
+    else 
+    {
+    echo "<$AccountName> account is hidden. Unhiding it now."
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name <$AccountName> -Value 1 -Force
+    }
+    
+    
