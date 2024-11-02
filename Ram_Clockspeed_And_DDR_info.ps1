cc101697aeb2e1b3ec5bc044d953fbe9ebaf89c3
cyber-jacob <cybervaquero@outlook.com>
2024-11-01 20:38:00 -0500

    added DDR5 to the key since we now live in the future

--- Ram_Clockspeed_And_DDR_info.ps1
+++ Ram_Clockspeed_And_DDR_info.ps1
@@ -1,14 +1,21 @@
+###########################################################################################################################################
+# Author:           Jacob Schweyer                                                                                                        #
+# Script Name:     Ram_clockspeed_and_ddr_info.ps1                                                                                        #
+# Description:     Determine Memory type of remote or local system by DDR schema. May be incomplete.                                      #
+###########################################################################################################################################
+
 try {
 $memorydetails=(get-ciminstance win32_physicalmemory); 
 echo $memorydetails
-echo("Configured clock by memory slot: 
+echo("Configured clock speed by memory slot: 
 `t" + $memorydetails.ConfiguredClockSpeed+ "")
 echo("SMBIOS Memory type by slot: 
 `t" + $memorydetails.SMBIOSMemorytype +"")
 echo ("SMBios Memory type key: 
 `t 21 = DDR2
 `t 24 = DDR3
-`t 26 = DDR4")
+`t 26 = DDR4)
+`t 34 = DDR5"
 }
 
 catch {
