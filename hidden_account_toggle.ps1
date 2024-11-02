e269c15d8713c4d98d4643fa3ba8f925b7baab6f
cyber-jacob <cybervaquero@outlook.com>
2024-11-01 20:40:58 -0500

    Changed script to contain read-host statement incase account_name var is not present ahead of time

--- hidden_account_toggle.ps1
+++ hidden_account_toggle.ps1
@@ -1,14 +1,15 @@
-﻿$HideValue = get-itemproperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList'| Select-Object -ExpandProperty <$AccountName>  
+﻿$AccountName = Read-Host("What is the account name")
+$HideValue = get-itemproperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList'| Select-Object -ExpandProperty <$AccountName>  
 
    if ($HideValue)
     {
     echo "<$AccountName> account is unhidden. Hiding it now."
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name <$AccountName> -Value 0 -Force
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name $AccountName -Value 0 -Force
     }
     else 
     {
     echo "<$AccountName> account is hidden. Unhiding it now."
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name <$AccountName> -Value 1 -Force
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name $AccountName -Value 1 -Force
     }
     
     
